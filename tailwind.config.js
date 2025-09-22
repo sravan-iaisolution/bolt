e43bfe1e34a9d43099c0062a8049fe9ab26d6d38
sravan-iaisolution <sravan.kumar@iaisolution.com>
2025-09-22 13:13:06 +0530

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+import forms from "@tailwindcss/forms"
+import typography from "@tailwindcss/typography"
+import aspectRatio from "@tailwindcss/aspect-ratio"
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {},
+  },
+  plugins: [forms, typography, aspectRatio],
+}
